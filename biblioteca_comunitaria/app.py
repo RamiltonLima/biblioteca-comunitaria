90cc03a94afec7c4c857371858bc4389632bc260
Ramilton Lima <ramilton.silva.lima@live.com>
2023-11-01 00:01:09 -0300

    Menu e layout basico

--- biblioteca_comunitaria/app.py
+++ biblioteca_comunitaria/app.py
@@ -1,381 +1,138 @@
 import streamlit as st
-from sqlalchemy import create_engine, or_, and_
-from sqlalchemy.orm import sessionmaker
-from models import HEROI_INONIMADO, Base, Livro, Leitor, Emprestimo, Ebook
+from streamlit_option_menu import option_menu
+from models import Livro, Usuario, Emprestimo, session
 import pandas as pd
-import numpy as np
-import re
-from io import BytesIO
-from datetime import datetime
-import zipfile
+
+from collections import namedtuple
+
+Aba = namedtuple('Aba',field_names=['nome_aba', 'titulo_aba'])
+
+class PaginaBase:
+    __nome = None
+    navegavel = True
+    __todas_subclasse = dict()
+    navegaveis = dict()
+    paginas = []
+
+    nomes_abas = []
+    
 
 
-engine = create_engine('sqlite:///estoque.db')
-Session = sessionmaker(bind=engine)
-session = Session()
+    @classmethod
+    @property
+    def nome(cls):
+        if cls.__nome is not None:
+            return cls.__nome
+        else: 
+            cls.__nome = cls.__name__
+            return cls.__nome
+        
 
-
-class Biblioteca():
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        if cls.navegavel:
+            PaginaBase.navegaveis[cls.nome] = cls
+            PaginaBase.paginas.append(cls.nome)
+        PaginaBase.__todas_subclasse[cls.nome] = cls
 
     def __init__(self) -> None:
+        st.header(self.nome,divider='orange')
 
-        st.set_page_config(
-            page_title="Qualiteca",
-            page_icon="📚",
-            layout="wide",
-            initial_sidebar_state="expanded",
-            menu_items={
-                'Get help': 'mailto:ramilton.silva.lima@live.com',
-                'Report a bug': "mailto:ramilton.silva.lima@live.com",
-                'About': "Para compartilhar livros"
-            }
-        )
 
+class Home(PaginaBase):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+
+class Estante(PaginaBase):
+    nome = 'Estande de Livros'
+    nomes_abas = ['Ver','Adicionar', 'Editar', 'Excluir']
+    
+
+    def __init__(self) -> None:
+        super().__init__()
         
+        abas = st.tabs(self.nomes_abas)
 
-        st.sidebar.title('Vamos lá!')
+        with abas[0]:
+            st.write('d')
+            st.dataframe(self.ver_estante())
 
-        self.secao = {
-            'Qualiteca': self.home,
-            'Emprestar': self.cadastrar_emprestimo,
-            'Devolver': self.terminar_emprestimo,
-            'Ver empréstimos': self.ver_emprestimos,
-            'Leitores: Adicionar': self.cadastrar_leitor,
-            'Leitores: Remover': self.remover_leitor,
-            'Leitores: Ver': self.ver_leitores,
-            'Livros: Adicionar': self.cadastrar_livro,
-            'Livros: Remover': self.remover_livro,
-            'Livros: Ver': self.ver_livros,
-            'Livros: Adicionar': self.cadastrar_livro,
-            'Livros: Remover': self.remover_livro,
-            'Livros: Ver': self.ver_livros,
-            'Ebook/PDF: Adicionar': self.cadastrar_ebooks,
-            'Ebook/PDF: Remover': self.remover_ebooks,
-            'Ebook/PDF: Ver': self.ver_ebooks,
-            'Backup dos dados' : self.backup_dados
-        }
+        with abas[1]:
+            self.adicionar_livro()
 
-        self.selecao = st.sidebar.radio(
-            'Vamos lá', options=list(self.secao.keys()))
+    def ver_estante(self):
+        dados = Livro.em_dataframe()
+        return dados
+    
+    def adicionar_livro(self):
 
-        pagina_selecionada = self.secao[self.selecao]
-        pagina_selecionada()
-
-    def home(self):
-        st.header('Qualiteca')
-        st.markdown('''Biblioteca formada com doações de livros do call center para incentivar a leitura de todos. Livros teremos o empréstimo de acordo como tamanho do livro, gibis e revistas ficarão dentro da descompressão para leitura rápida quando estiverem lá.''')
-
-    def __validar_email(self, email):
-        regex = re.compile(
-            r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
-
-        if re.fullmatch(regex, email):
-            return True
-        else:
-            return False
-
-    def __dataframes(self, consulta):
-
-        df_consulta = pd.DataFrame([item.representacao() for item in consulta])
-
-        if df_consulta.empty:
-            st.write('...Na verdade, não há nada aqui ainda.')
-        else:
-            st.dataframe(df_consulta, hide_index=True, use_container_width=True)
-
-    def ver_livros(self):
-        st.header('Estes são os livros que temos.')
-        todos_livros = session.query(Livro).all()
-        self.__dataframes(todos_livros)
-
-    def cadastrar_livro(self):
-        st.header('Legal! Mais um livro!')
-
-        nome = st.text_input('Nome do livro', max_chars=500,
-                             help="Coloque o nome completo da obra")
-        apoiador_nome = st.text_input(
-            'Nome do apoiador', max_chars=200, help="Aquele responsável por esta aquisição")
-        apoiador_email = st.text_input(
-            'E-mail do apoiador', max_chars=200, help="E-mail dquele responsável por esta aquisição")
-
-        if st.button('Guardar livro'):
-
-            if nome:
-                novo_livro = Livro(
-                    nome=nome,
-                    apoiador_nome=apoiador_nome,
-                    apoiador_email=apoiador_email
-                )
-                session.add(novo_livro)
-                session.commit()
-                st.success(f'''
-                Valeu! {apoiador_nome if apoiador_nome else HEROI_INONIMADO }! 
-                "{ nome }" cadastrado com sucesso!
-            ''')
+        coluna1, coluna2 = st.columns(2)
+        with coluna1:
+            foto_ou_imagem = st.radio('Vamos pegar uma imagem do livro.', options=['Carregar imagem','Abrir camera'])
+            
+            if foto_ou_imagem == 'Carregar imagem':
+                imagem_livro = st.file_uploader('Imagem do livro', type=[])
             else:
-                st.error('Preciso saber o nome do livro')
-
-    def remover_livro(self):
-        st.header('Menos um livro')
-        livro_id = st.number_input(
-            "Qual o ID do livro que deseja remover?", step=0, value=None)
-
-        if not st.button('Remover este livro'):
-            pass
-
-        elif not livro_id:
-            st.error(f'Preciso saber qual livro quer remover.')
-
-        else:
-            livro = session.query(Livro).filter(Livro.id == livro_id).first()
-            if not livro:
-                st.error(
-                    f'Não foi encontrado nenhum livro com o ID {livro_id}.')
-            else:
-                emprestimos = session.query(Emprestimo).filter(
-                    Emprestimo.livro_id == livro_id).all()
-                for emprestimo in emprestimos:
-                    session.delete(emprestimo)
-
-                session.delete(livro)
-                session.commit()
-
-                st.success(
-                    f'O livro com ID {livro_id} - "{livro.nome}" e todos os empréstimos vinculados foram removidos com sucesso.')
-
-    def ver_leitores(self):
-        st.header('Todos os leitores cadastrados')
-        todos_leitores = session.query(Leitor).all()
-        self.__dataframes(todos_leitores)
-
-    def cadastrar_leitor(self):
-        st.header('Opa! Bem vindo!')
-
-        nome = st.text_input('Nome do Leitor', max_chars=500)
-        email = st.text_input('E-mail do Leitor', max_chars=200)
-
-        if st.button('Cadastrar este Leitor'):
-
-            if nome and email and self.__validar_email(email=email):
-                novo_leitor = Leitor(
-                    nome=nome,
-                    email=email
-                )
-                session.add(novo_leitor)
-                session.commit()
-                st.success(
-                    f'''Show! {nome}({email}) cadastrado com sucesso!''')
-            else:
-                st.error(
-                    'Realmente é necessário saber o nome e o e-mail válido do novo leitor')
-
-    def remover_leitor(self):
-        st.header('Que pena...')
-        leitor_id = st.number_input(
-            "Qual o ID de quem sairá?", step=0, value=None)
-
-        if not st.button('Remover esta pessoa'):
-            pass
-
-        elif not leitor_id:
-            st.error(f'Preciso saber qual pessoa quer remover.')
-
-        else:
-            leitor = session.query(Leitor).filter(
-                Leitor.id == leitor_id).first()
-            if not leitor:
-                st.error(f'Não foi encontrado ninguém com o ID {leitor_id}.')
-            else:
-                emprestimos = session.query(Emprestimo).filter(
-                    Emprestimo.leitor_id == leitor_id).all()
-                for emprestimo in emprestimos:
-                    session.delete(emprestimo)
-
-                session.delete(leitor)
-                session.commit()
-
-                st.success(
-                    f'{leitor.nome}({leitor_id}) e todos os empréstimos vinculados foram removidos com sucesso.')
-
-    def ver_emprestimos(self):
-        st.header('Livros emprestados.')
-        todos_emprestimos = session.query(Emprestimo).all()
-
-        emprestimos = pd.DataFrame([item.representacao()
-                                   for item in todos_emprestimos])
-
-        if emprestimos.empty:
-            st.write('Ops. Na verdade, nada aqui ainda.')
-        else:
-            mostrar_devolvido = st.checkbox('Mostar somente pendentes')
-            emprestimos['Atrasado'] = emprestimos['Devolver em'].apply(lambda x: x < datetime.now().date())
-            if not mostrar_devolvido:
-                st.dataframe(emprestimos, hide_index=True)
-            else:
-                somente_pendentes = emprestimos[emprestimos['Devolvido'] == False].copy()
-                st.dataframe(somente_pendentes, hide_index=True)
-
-    def cadastrar_emprestimo(self):
-        st.header('Boa! Mas atenção no prazo.')
-
-        livros_sem_emprestimo = session.query(Livro).outerjoin(Emprestimo).group_by(Livro).having(or_(Emprestimo.id == None, and_(Emprestimo.terminado == True))).all()
-
-        leitores = session.query(Leitor)
-
-        if not livros_sem_emprestimo:
-            st.error('Ainda não temos livros')
-        elif not leitores:
-            st.error('Ainda não há quem ler')
-
-        else:
-            livro = st.selectbox('Qual livro?', livros_sem_emprestimo)
-            devolver_em = st.date_input(
-                'Vai devolver em?', min_value=datetime.today())
-            leitor = st.selectbox('Quem?', leitores)
-
-            if st.button("Pegar emprestado"):
-
-                if not livro or not leitor or not devolver_em:
-                    st.error("Vamos lá, é simples, Qual? Quando? Quem?")
-
-                else:
-                    novo_emprestimo = Emprestimo(
-                        leitor_id=leitor.id,
-                        livro_id=livro.id,
-                        data_inicio=datetime.now().date(),
-                        data_fim=devolver_em
-                    )
-                    session.add(novo_emprestimo)
-                    session.commit()
-                    st.success(f'Pronto! o "{livro.nome}" foi emprestado para {leitor}, de {datetime.now().date()} até {devolver_em}')
-
-    def terminar_emprestimo(self):
-        st.header('Obrigado! Parabéns! Volte sempre.')
-        emprestimo_id = st.number_input(
-            "Qual o id do emprestimo?", step=0, value=None)
-
-        if not st.button('Devolver o empréstimo'):
-            pass
-        elif not emprestimo_id:
-            st.error(f'Informe o numero do empréstimo')
-
-        else:
-            emprestimo = session.query(Emprestimo).filter(
-                Emprestimo.id == emprestimo_id, Emprestimo.terminado == False).first()
-            if not emprestimo:
-                st.error(
-                    f'Não foi possivel encontrar nenhum emprestimo em aberto com este id')
-            else:
-                emprestimo.terminado = True
-                emprestimo.data_fim = datetime.now().date()
-                session.commit()
-                st.success(f'Obrigado! Devolvido com sucesso')
+                foto_livro = st.camera_input('Adicione uma foto do livro')
+            
+        with coluna2:
+            nome_livro = st.text_input('Qual nome do livro?')
+            st.caption('Caso o doador não esteja na lista, cadastre-o antes, na seção de "Pessoas"')
+            doador = st.selectbox('Quem está doando?', options=['Ninguem'])
+            prazo_emprestimo = st.number_input('Quantos dias será o emprestimo do livro', min_value=1)
 
 
-    def ver_ebooks(self):
-        def baixar():
-            print('olá')
-
-        st.header('Estes da para baixar sem precisar devolver')
-
-        ebook_id = st.number_input("Qual o ID do PDF que quer baixar?", step=0, value=None)
-        botao = st.button("Procurar ebook", on_click=baixar, key='procurar_ebook')
-        if botao:
-            ebook = session.query(Ebook).filter(Ebook.id == ebook_id).first()
-            if not ebook:
-                st.error("Nenhum ebook encontrado com este ID")
-            else:
-                st.success('Encontrado')
-                pdf_bytes = ebook.conteudo
-                st.download_button(
-                    label="Baixar",
-                    data=pdf_bytes,
-                    file_name=f"{ebook.nome}",
-                    mime="application/pdf",
-                )
-
-        st.divider()
-        st.header('Listagem de ebooks')
-        todos_ebooks = session.query(Ebook).all()
-        self.__dataframes(todos_ebooks)
-
-        
-    def cadastrar_ebooks(self):
-        st.header('Compartilhe seu ebook')
-        ebook = st.file_uploader('Suba seu arquivo PDF', type=['pdf'])
-
-        if st.button('Guardar ebook'):
-            if not ebook:
-                st.error('Nenhum arquivo para guardar')
-            else:
-                conteudo = ebook.read()
-                novo_ebook = Ebook(nome=ebook.name, conteudo=conteudo)
-                session.add(novo_ebook)
-                session.commit()
-                st.success('Pronto! PDF Guardado com sucesso')
-
-    def remover_ebooks(self):
-        st.header('Ok. Ninguem mais vai ler este')
-        ebook_id = st.number_input(
-            "Qual o ID do ebook", step=0, value=None)
-
-        if not st.button('Remover Ebook'):
-            pass
-
-        elif not ebook_id:
-            st.error(f'Preciso saber qual ebook quer remover.')
-
-        else:
-            ebook = session.query(Ebook).filter(
-                Ebook.id == ebook_id).first()
-            if not ebook:
-                st.error(f'Não foi encontrado nenhum ebook com o ID {ebook_id}.')
-            else:
-                session.delete(ebook)
-                session.commit()
-
-                st.success(
-                    f'{ebook.nome}({ebook.id}) removido com sucesso')
-
-    def backup_dados(self):
-        st.header('Baixe os dados da Biblioteca, quem sabe precise...')
-
-        todos_livros = session.query(Livro).all()
-        todos_leitores = session.query(Leitor).all()
-        todos_emprestimos = session.query(Emprestimo).all()
-
-        def gerar_botao(consulta, nome_arquivo):
-            buffer = BytesIO()
-            dados = [item.backup() for item in consulta]
-            df = pd.DataFrame(dados)
-            if not df.empty:
-                df.to_csv(buffer, index=False, encoding='utf-8-sig', sep=';')
-
-                st.download_button(
-                    label=nome_arquivo,
-                    data=buffer,
-                    file_name=f'{nome_arquivo}.csv',
-                )
-
-        gerar_botao(todos_livros, 'Livros')
-        gerar_botao(todos_leitores, 'Leitores')
-        gerar_botao(todos_emprestimos, 'Emprestimos')
 
 
-        ebooks = session.query(Ebook).all()
-        if ebooks:
-            buffer_todos_ebooks = BytesIO()
-            with zipfile.ZipFile(buffer_todos_ebooks, "w", zipfile.ZIP_DEFLATED, False) as zip_file:
-                for ebook in ebooks:
-                    zip_file.writestr(f"{ebook.id}-{ebook.nome}", ebook.conteudo)
 
-            st.download_button(
-                label="Ebooks",
-                data=buffer_todos_ebooks,
-                file_name="ebooks.zip",
-                mime="application/zip"
+class Pessoas(PaginaBase):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+class Empréstimos(PaginaBase):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+class Dados(PaginaBase):
+    nome = 'Gestão dos dados'
+
+    def __init__(self) -> None:
+        super().__init__()
+        st.button('teste')
+
+
+
+
+
+class Biblioteca:
+    def __init__(self) -> None:
+        self.menu()
+
+
+    def menu(self):
+        with st.sidebar:
+            destino = option_menu(
+                menu_title="Vamos lá!",
+                menu_icon="cast",
+                default_index=0,
+                orientation="vertical",
+                options=PaginaBase.paginas,
+                styles={
+                    "container": {"background-color": "transparent"}
+                }
             )
+        PaginaBase.navegaveis[destino]()
 
 
-
-biblioteca = Biblioteca()
+Biblioteca()
+    # menu_icon="cast", default_index=0, orientation="horizontal",
+    # styles={
+    #     "container": {"padding": "0!important", "background-color": "#fafafa"},
+    #     "icon": {"color": "orange", "font-size": "25px"}, 
+    #     "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+    #     "nav-link-selected": {"background-color": "green"},
+    # }
--- biblioteca_comunitaria/models.py
+++ biblioteca_comunitaria/models.py
@@ -1,112 +1,141 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Date, LargeBinary, Boolean
-from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Date, DateTime, LargeBinary, Boolean, Enum
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+import pandas as pd
 
-HEROI_INONIMADO = "Herói Inonimado"
+engine = create_engine('sqlite:///biblioteca.db')
+Session = sessionmaker(bind=engine)
+session = Session()
 
 Base = declarative_base()
 
-class Leitor(Base):
-    __tablename__ = 'leitores'
+class ModeloBase(Base):
+    __abstract__ = True
     id = Column(Integer, primary_key=True)
+    registrado_em = Column(DateTime, nullable=False, default=datetime.now)
+    ultima_edicao_em = Column(DateTime, nullable=True)
+    modificado = Column(Boolean, nullable=False, default=False)
+    excluido_em = Column(DateTime, nullable=True)
+    excluido = Column(Boolean, nullable=False, default=False)
+
+    @classmethod
+    def retornar(cls, campo='id', valor=None, unico_registro=True, session=session):
+        filtro = True if valor is None else getattr(cls, campo) == valor
+        if unico_registro is None:
+            return session.query(cls).filter(filtro, (not cls.excluido)).first()
+        else:
+            return session.query(cls).filter(filtro, (not cls.excluido)).all()
+
+    @classmethod
+    def editar(cls, campo='id', valor=None, campo_edicao=None, novo_valor=None, unico_registro=True, session=session):
+         
+        objeto = cls.retornar(campo=campo, valor=valor, unico_registro=unico_registro, session=session)
+
+        if not objeto:
+            return None
+        elif isinstance(objeto, list):
+            editando = objeto
+        else:
+            editando = [objeto]
+            setattr(objeto, campo, novo_valor)
+
+        editados = []
+        for item in editando:
+            setattr(item, campo_edicao, novo_valor)
+            setattr(item, 'ultima_edicao_em', datetime.now())
+            setattr(item, 'modificado', True)
+            session.commit()
+            editados.append(item)
+
+        return editados
+        
+    @classmethod
+    def excluir(cls, campo='id', valor=None, unico_registro=True, session=session):
+        cls.editar(
+            campo=campo,
+            valor=valor,
+            campo_edicao='excluido_em',
+            novo_valor=datetime.now(),
+            unico_registro=unico_registro,
+            session=session
+        )
+
+        cls.editar(
+            campo=campo,
+            valor=valor,
+            campo_edicao='excluido_por',
+            unico_registro=unico_registro,
+            session=session
+        )
+        
+        return cls.editar(
+            campo=campo,
+            valor=valor,
+            campo_edicao='excluido',
+            novo_valor=True,
+            unico_registro=unico_registro,
+            session=session
+        )
+
+    @classmethod
+    def em_dataframe(cls, campo=None, valor=None, session=session):
+        
+        objetos = cls.retornar(campo=campo, valor=valor, unico_registro=False, session=session)
+
+        objetos_dict = [objeto.em_dict() for objeto in objetos]
+        df = pd.DataFrame(objetos_dict)
+
+        return pd.DataFrame(objetos_dict)
+
+    
+
+    def em_dict(self):
+        objeto_dict = {}
+        for attr, value in self.__dict__.items():
+            objeto_dict[attr] = value
+        return objeto_dict
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__}(id={self.id})>"
+
+
+class Usuario(ModeloBase):
+    __tablename__ = 'usuarios'
+
     nome = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
+
     emprestimos = relationship('Emprestimo', back_populates='leitor')
+    doacao = relationship('Livro', back_populates='doador')   
 
-    # TODO: Registrar quem e quando cadastrou este leitor
 
-    def representacao(self):
-        return {'ID': self.id, 'Nome': self.nome, 'E-mail': self.email}
-    
-    def backup(self):
-        return self.representacao()
-    
-    def __str__(self):
-        return f'({self.id}) {self.nome}'
-
-class Livro(Base):
+class Livro(ModeloBase):
     __tablename__ = 'livros'
-    id = Column(Integer, primary_key=True)
-    nome = Column(String, nullable=False)
-    apoiador_nome = Column(String)
-    apoiador_email = Column(String)
+    tempo_emprestimo_padrao = lambda : 1
+
+    titulo = Column(String, nullable=False)
+    doador_id = Column(Integer, ForeignKey('usuarios.id'), nullable=False)
+    foto_capa = Column(LargeBinary, nullable=False)
+    extensao_foto_capa = Column(String, nullable=False)
+    tempo_emprestimo = Column(Integer, nullable=False, default=tempo_emprestimo_padrao)
+
+    doador = relationship('Usuario', back_populates='doacao')
     emprestimos = relationship('Emprestimo', back_populates='livro')
+
     
-    # TODO: Registrar quem e quando guardou cada livro
-
-    def representacao(self):
-
-        apoiador_nome = self.apoiador_nome if self.apoiador_nome else HEROI_INONIMADO
-
-        return {
-            'ID': self.id,
-            'Nome': self.nome,
-            'Nome do Apoiador': apoiador_nome,
-            'E-mail do Apoiador': self.apoiador_email
-        }
-
-    def backup(self):
-        return self.representacao()
-    
-    def __str__(self):
-        return f'({self.id}) {self.nome}'
-
-        
 
 
-
-class Emprestimo(Base):
+class Emprestimo(ModeloBase):
     __tablename__ = 'emprestimos'
-    id = Column(Integer, primary_key=True)
-    leitor_id = Column(Integer, ForeignKey('leitores.id'), nullable=False)
+
+    leitor_id = Column(Integer, ForeignKey('usuarios.id'), nullable=False)
     livro_id = Column(Integer, ForeignKey('livros.id'), nullable=False)
     data_inicio = Column(Date, nullable=False)
     data_fim = Column(Date, nullable=False)
     terminado = Column(Boolean, nullable=False, default=False)
 
-    leitor = relationship('Leitor', back_populates='emprestimos')
+    leitor = relationship('Usuario', back_populates='emprestimos')
     livro = relationship('Livro', back_populates='emprestimos')
 
-    def representacao(self):
-        return {
-            'ID': self.id,
-            'Leitor': f'{self.leitor.nome}({self.leitor_id})',  
-            'Livro': f'{self.livro.nome}{(self.livro_id)}',
-            'Retirada em': self.data_inicio,
-            'Devolver em': self.data_fim,
-            'Devolvido' : self.terminado
-        }
-
-
-    def backup(self):
-        return {
-            'ID': self.id,
-            'Leitor ID':self.leitor.id,
-            'Leitor': self.leitor.nome,
-            'Livro ID': self.livro_id,
-            'Livro': self.livro.nome,
-            'Retirada em': self.data_inicio,
-            'Devolver em': self.data_fim,
-            'Devolvido' : self.terminado
-        }
-
-class Ebook(Base):
-    __tablename__ = 'ebooks'
-    id = Column(Integer, primary_key=True)
-    nome = Column(String, nullable=False)
-    conteudo = Column(LargeBinary, nullable=False)
-
-    def __repr__(self):
-        return f"<Ebook(titulo='{self.titulo}')>"
-    
-
-    def representacao(self):
-        return {
-            'ID': self.id,
-            'Nome': self.nome,
-        }
-    
-
-
-engine = create_engine('sqlite:///estoque.db')
 Base.metadata.create_all(engine)
